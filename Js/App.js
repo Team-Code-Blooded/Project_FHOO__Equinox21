8ad28c9365c1f5b62d5b31c24dcf6c2ee51d928a
Abhii-Agarwal09 <abhishek.agarwal.work20@gmail.com>
2021-05-01 16:46:17 +0530

    volunteer and virtual pet page added, code refactored

---
+++ Js/App.js
@@ -0,0 +1,68 @@
+// Dropdown animation on navigation bar 
+const dropMenu = document.getElementsByClassName("dropdown");
+const dropMenu1 = document.getElementsByClassName("dropdown1");
+const submit = document.getElementById("submit");
+const msg = document.getElementById("msg");
+const form = document.getElementById("form");
+
+function myFunction_1(event) {
+    event.stopPropagation();
+    dropMenu[0].classList.toggle("show");
+    dropMenu[1].classList.remove("show");
+}
+
+function myFunction_2(event) {
+    event.stopPropagation();
+    dropMenu[1].classList.toggle("show");
+    dropMenu[0].classList.remove("show");
+}
+
+function myFunction_3(event) {
+    event.stopPropagation();
+    dropMenu1[0].classList.toggle("show");
+}
+
+// Close dropdown when clicked anywhere on window
+window.onclick = function(event) {
+    dropMenu[0].classList.remove("show");
+    dropMenu[1].classList.remove("show");
+    dropMenu1[0].classList.remove("show");
+}
+
+function Redirect_home() {
+    location.href = "/HTML/home_page.html";
+}
+
+function Redirect_login() {
+    location.href = "/HTML/login_page.html";
+}
+
+function Redirect_afterlogin() {
+    location.href = "/HTML/afterlogin_homepage.html";
+}
+
+function Redirect_signup() {
+    location.href = "/HTML/signup.html";
+}
+
+function Redirect_medical() {
+    location.href = "/HTML/medical_requirement.html";
+}
+
+function Redirect_blood() {
+    location.href = "/HTML/blood_bank.html";
+}
+
+function Redirect_food() {
+    location.href = "/HTML/food_bank.html";
+}
+
+function Redirect_userdashboard() {
+    location.href = "/HTML/user_dashboard.html";
+}
+
+// Success message and reset form
+submit.addEventListener("click", function() {
+    msg.innerHTML = "Your Message has been sent successfully.";
+    form.reset();
+});
